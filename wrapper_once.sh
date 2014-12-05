b43b042a5fdd6050fcb7a0a66bfe44bf340ad6ca
Blake Caldwell <caldweba@colorado.edu>
2014-12-05 00:47:40 -0700

    separate wrappers for each node and global

--- wrapper_once.sh
+++ wrapper_once.sh
@@ -14,8 +14,7 @@ TIMEOUT=10
 #echo ${SLUM_NODELIST} |sed 's/node//g' | sed 's/[\[,]\([0-9]\+\)\-\([0-9]\+\)\]/{\2..\1}/g' | sed 's/,/ /'
 
 cd /projects/caldweba/
-#/projects/caldweba/dump_fts -n -t ${TIMEOUT} > fts.out.${START_TIME}
-
+/projects/caldweba/dump_fts -n -t ${TIMEOUT} > fts.out.${START_TIME}
 scontrol show hostname ${SLUM_NODELIST} > job_nodes
 ibnetdiscover -p -t ${TIMEOUT} |grep -f job_nodes|grep -e "^CA" |awk '{print $2}' > /projects/caldweba/lids_of_job_members
 for each in $(cat lids_of_job_members); do
@@ -23,14 +22,15 @@ for each in $(cat lids_of_job_members); do
   echo $OUTPUT
 done | sed 's/.*\.\.\([0-9]\+\) .*\.\.\([0-9]\+\) .*\.\.\([0-9]\+\) .*\.\.\([0-9]\+\)/\1:\2:\3:\4/' > neighbors.${START_TIME}
 
-
 # Balance the queries
 python balance_queries.py --forwardingdb fts.out.${START_TIME} --neighbors neighbors.${START_TIME} > balance.out
 
-#**** These should be in the job submission script ****
-# Split into per-node files
-split_queries.sh
+#cd /projects/caldweba/
+#split_queries.sh
+# This will take balance.out and split it into the individual files the node is responsible for in 
+#  /projects/caldweba/perfqueries/
+
 #spawn_queries.sh &
-# The last command will spawn N jobs in the background (where N is the number of perfqueries, and then wait
-# until that sweep is complete. It writes the output to /projects/caldweba/logs/
-# It will loop forever until it is killed (by the slurm script)
+# This will spawn N jobs in the background (where N is wc -l perfqueries/$HOSTNAME_perfqueries
+#   and then wait until that sweep is complete. It writes the output to /projects/caldweba/logs/
+#   It will loop forever until it is killed (by the slurm script)
