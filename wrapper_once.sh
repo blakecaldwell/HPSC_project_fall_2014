ae4514f1d42c571823009a6b8950c697e0874dce
Blake Caldwell <caldweba@colorado.edu>
2014-12-05 00:49:18 -0700

    cleaning up wrapper_once.sh

--- wrapper_once.sh
+++ wrapper_once.sh
@@ -6,12 +6,9 @@ die() {
 }
 
 START_TIME=$(date '+%Y%m%d%H%M')
-# in ms
-TIMEOUT=10
+TIMEOUT=10 # in ms
 
 [ $SLURM_NODELIST ] || die "are you running outside of SLURM?"
-
-#echo ${SLUM_NODELIST} |sed 's/node//g' | sed 's/[\[,]\([0-9]\+\)\-\([0-9]\+\)\]/{\2..\1}/g' | sed 's/,/ /'
 
 cd /projects/caldweba/
 /projects/caldweba/dump_fts -n -t ${TIMEOUT} > fts.out.${START_TIME}
@@ -24,13 +21,3 @@ done | sed 's/.*\.\.\([0-9]\+\) .*\.\.\([0-9]\+\) .*\.\.\([0-9]\+\) .*\.\.\([0-9
 
 # Balance the queries
 python balance_queries.py --forwardingdb fts.out.${START_TIME} --neighbors neighbors.${START_TIME} > balance.out
-
-#cd /projects/caldweba/
-#split_queries.sh
-# This will take balance.out and split it into the individual files the node is responsible for in 
-#  /projects/caldweba/perfqueries/
-
-#spawn_queries.sh &
-# This will spawn N jobs in the background (where N is wc -l perfqueries/$HOSTNAME_perfqueries
-#   and then wait until that sweep is complete. It writes the output to /projects/caldweba/logs/
-#   It will loop forever until it is killed (by the slurm script)
